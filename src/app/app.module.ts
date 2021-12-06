af6175bbbdc7cd5962a2eda2f17a6a637ecf32ad
Sibonelo-Vilakazi <sibonelo@monkeyandriver.com>
2021-12-06 22:35:56 +0200

    removed the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { JobApplicationComponent } from './pages/job-application/job-application.component';
 import { CongradulationsPageComponent } from './pages/congradulations-page/congradulations-page.component';
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +14,7 @@ import { CongradulationsPageComponent } from './pages/congradulations-page/congr
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
